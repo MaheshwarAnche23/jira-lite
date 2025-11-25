1ae19e855e8ac12230a90cc3c9c900a5a7694842
Maheshwar Anche <maheshwar.anche@capgemini.com>
2025-11-25 15:42:33 +0530

    Initialized Spring Boot backend

---
+++ backend/jira-lite-backend/src/main/java/com/jiralite/JiraLiteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jiralite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiraLiteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiraLiteBackendApplication.class, args);
+	}
+
+}
---
+++ backend/jira-lite-backend/src/test/java/com/jiralite/JiraLiteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiralite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiraLiteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
